45c5f0387edf7874640579a8fdd32fe45ee8a209
nielsreijnders <niels.reijnders@edu-kw1c.nl>
2019-02-19 10:00:30 +0100

    commit

--- OPDRACHT2.1/OPDRACHT2.1.cpp
+++ OPDRACHT2.1/OPDRACHT2.1.cpp
@@ -1,5 +1,14 @@
-// OPDRACHT2.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/************************** Module Header *******************************\
+Opdracht:       opdracht5
+Auteur:         Niels Reijnders
+Aanmaakdatum:   13-02-2019    09:38
+Bestandsnaam:   opdracht3.1.cpp
+Versie:         0.1
+				0.2
+				1.0
+
+\************************************************************************/
+
 
 
 #include "pch.h"
@@ -9,14 +18,3 @@ int main()
 {
     std::cout << "Hello World!\n"; 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

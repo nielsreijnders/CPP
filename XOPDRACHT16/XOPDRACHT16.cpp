054508e8e4d56520418834728d82eeb8d1af1bd0
nielsreijnders <niels.reijnders@edu-kw1c.nl>
2019-03-19 10:23:47 +0100

    Huiswerk CPP 💪🏼

---
+++ XOPDRACHT16/XOPDRACHT16.cpp
@@ -0,0 +1,12 @@
+// XOPDRACHT16.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
